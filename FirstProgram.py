f8baf655fb36e57f8d9583d87f55ae4cee262d40
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-05-18 11:14:50 +0530

    add new file & update old

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,2 +1,10 @@
 print ("This is my first program")
 print ("Hello World")
+name = "Ankit" 
+age = 26
+
+age2 =age + 1
+
+print ("My name is ", name)
+print ("My age is ", age)
+print ("My age next year will be ", age2)
