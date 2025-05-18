82fbc979391567057d16b02327258e6c76e71674
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-05-18 12:10:18 +0530

    update both

--- FirstProgram.py
+++ FirstProgram.py
@@ -2,9 +2,17 @@ print ("This is my first program")
 print ("Hello World")
 name = "Ankit" 
 age = 26
-
-age2 =age + 1
-
+age2 = age + 1
+price = 76.6
+old = True
+a= None
 print ("My name is ", name)
 print ("My age is ", age)
 print ("My age next year will be ", age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(age2))
+print(type(old))
+print(type(a))
