b908f2e2f003903de9c1fea188ddd3bd0d6f461b
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-05-19 13:15:28 +0530

    Practice

---
+++ Practice qustion.py
@@ -0,0 +1,16 @@
+# qus: 1
+frist = int(input ("Frist number:" ))
+second = int(input ("Second number:" )) 
+print ("sum =", frist + second) 
+# qus: 2
+side = int(input("Enter the length of side of square: "))
+area = side * side
+print("Area of the square is:", area)   
+# qus: 3
+a = float(input ("frist number :"))
+b = float(input ("second number :")) 
+print ("avg =", (a+b)/2)
+# qus: 4
+a = int(input ("frist number :"))
+b = int(input ("second number :"))
+print (a>=b)
