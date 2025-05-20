0f60a1b9b8a7ebb9c4e3fe2119ca0a10f0cf5892
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-05-20 17:28:44 +0530

    new file add

---
+++ list & tapals thory.py
@@ -0,0 +1,12 @@
+marks = [ 87,34,45,542,23,45,67,89,90]
+print("marks:", marks)
+print(len(marks))
+print(marks[0])
+print(marks[1])
+
+students = ["Ankit",86, "Amit", 45, "Anil", 78, "Ajay", 90]
+print("students:", students)
+print(students[0])
+students[0] = "Bhavik"
+print(students)
+
