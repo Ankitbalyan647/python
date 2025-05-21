145ab7bbd17685176b6868b9f8d3a82d75ac59d8
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-05-21 11:22:12 +0530

    done some new qus

--- Practice qustion.py
+++ Practice qustion.py
@@ -36,4 +36,29 @@ if (a%7==0):
     print ("Number is multiple of 7")
 else:
     print ("Nummber is not multiple of 7")
-    
+# qus: 8
+movies = []
+movies.append(input("Enter 1st movie name: "))
+movies.append(input("Enter 2nd movie name: "))
+movies.append(input("Enter 3rd movie name: "))
+print("Movies:", movies)
+
+# qus: 9
+list1 = [1,2,1]
+
+copy_list = list1.copy()
+copy_list.reverse()
+
+if(copy_list == list1):
+    print("List1 is palindrome")
+else:
+    print("List1 is not palindrome")
+
+# qus: 10
+tup = ("C","D","A","A","B","B","A")
+print(tup.count("A"))
+ 
+# qus: 11
+tup = ["C","D","A","A","B","B","A"]
+tup.sort()
+print(tup)
