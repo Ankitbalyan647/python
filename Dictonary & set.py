b70d62acf4a79f363b238105dda24a418d1c471e
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-05-21 11:55:45 +0530

    update

---
+++ Dictonary & set.py
@@ -0,0 +1,21 @@
+info = {
+    "key": "value",
+    "subjact" :["Python", "Java", "C++"],
+    "topics" :("dic", "set", "list"),
+    "name" :"Ankit",
+    "age" :25,
+    "is student" :True,
+    "marks" :[ 34, 45, 542, 23, 45, 67, 89, 90],
+}
+info["name"] = "Bhavik"
+info["surname"] = "Kadyan"
+print(info)
+print(info["key"])
+print(info["subjact"])
+print(info["topics"])
+print(info["name"])
+
+null_dict = {}
+null_dict["name"] = "Ankit"
+null_dict["age"] = 25
+print(null_dict)
