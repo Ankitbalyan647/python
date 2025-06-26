968941b60b1e902a3e02dc1d5005ce8563dcfd76
Ankitbalyan647 <ankitbalyan647@gmail.com>
2025-06-26 20:28:52 +0530

    new_project

---
+++ new_project.py
@@ -0,0 +1,8 @@
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+#don't change the code below
+print(f"lollilfred's health is: {player_health}")
+print(f"lollilfred's is hit by a sword for {sword_damage} damage...")
+print(f"lollilfred's health after the attack is: {health_after_attack}")
